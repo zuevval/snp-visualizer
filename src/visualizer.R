320e623d39110e6ee9986ad20b20ab01b3285cd3
zuevval <valera.zuev.zva@gmail.com>
2020-11-02 18:59:53 +0300

    python: add converter for Gephi

--- src/visualizer.R
+++ src/visualizer.R
@@ -4,12 +4,14 @@ load_dist_mtx = function(input_filename = ? character) {
   read.table(input_filename)
 }
 
-heatmap = function(dm = ? data.frame){
+heatmap = function(dm = ? data.frame, show_values = TRUE){
   # credit: https://stackoverflow.com/a/3082602
   dst <- data.matrix(dm)
   dim <- ncol(dst)
   image(1:dim, 1:dim, dst, axes = FALSE, xlab="", ylab="")
-  text(expand.grid(1:dim, 1:dim), sprintf("%0.1f", dst), cex=0.6)
+  if(show_values){
+    text(expand.grid(1:dim, 1:dim), sprintf("%0.1f", dst), cex=0.6)
+  }
 }
 
 force_directed_graph = function(dm = ? data.frame, out_filename = ? character){
@@ -35,3 +37,8 @@ lapply(c(10, 20, 30, 40, 50), measure_time)
 dm <- load_dist_mtx("../data/out/test/snp_matrix.txt")
 system.time(heatmap(dm))
 system.time(force_directed_graph(dm, "../data/out/vis/test_force_graph.jpg"))
+
+setwd("E:/Users/vzuev/github-zuevval/snp-visualizer/src")
+dm <- load_dist_mtx("../data/out/real_data/snp_matrix.txt")
+system.time(force_directed_graph(dm, "../data/out/vis/real_data/force_graph.jpg"))
+system.time(heatmap(dm), FALSE)
